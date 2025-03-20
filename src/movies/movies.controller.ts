4a33aed757849a97011a9ca651f997be1f983bc8
SuperMK292 <33517890+SuperMK292@users.noreply.github.com>
2025-03-20 05:27:37 +0200

    added the CRUD operations for Movie entity

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './movie.entity';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Post()
+  create(@Body() movieData: Partial<Movie>): Promise<Movie> {
+    return this.moviesService.create(movieData);
+  }
+
+  @Get()
+  findAll(): Promise<Movie[]> {
+    return this.moviesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<Movie | null> {
+    return this.moviesService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() movieData: Partial<Movie>): Promise<Movie> {
+    return this.moviesService.update(id, movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<void> {
+    return this.moviesService.remove(id);
+  }
+}
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Movie } from './movie.entity';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(
+    @InjectRepository(Movie)
+    private movieRepository: Repository<Movie>,
+  ) {}
+
+  // Create a new movie
+  async create(movieData: Partial<Movie>): Promise<Movie> {
+    const movie = this.movieRepository.create(movieData);
+    return this.movieRepository.save(movie);
+  }
+
+  // Get all movies
+  async findAll(): Promise<Movie[]> {
+    return this.movieRepository.find();
+  }
+
+  // Get a movie by ID
+  async findOne(id: number): Promise<Movie | null> {
+    return this.movieRepository.findOne({ where: { id } });
+  }
+
+  // Update movie details
+  async update(id: number, movieData: Partial<Movie>): Promise<Movie> {
+    await this.movieRepository.update(id, movieData);
+    return this.findOne(id);
+  }
+
+  // Delete a movie
+  async remove(id: number): Promise<void> {
+    await this.movieRepository.delete(id);
+  }
+}
